0deda936e76e858649d54b8a7165ff4771598092
GurinderS120 <gurinder00singh11@gmail.com>
2025-02-20 12:06:58 -0800

    chore(analysis): update an import statement for consistency purposes

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -11,7 +11,7 @@ This script:
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
+from matplotlib import gridspec
 import seaborn as sns
 
 def summarize_activity(df: pd.DataFrame) -> pd.DataFrame:
